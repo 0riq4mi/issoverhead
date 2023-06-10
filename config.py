4e67523e4d08f074558854518a0340360aff6869
Oktay Caylak <0riq4mi@gmail.com>
2023-06-11 00:27:42 +0300

    iss-over-head

---
+++ config.py
@@ -0,0 +1,5 @@
+MY_EMAIL = "example@gmail.com"  # Your email
+MY_PASSWORD = "123456"  # Your password or Application password(from your mail service)
+MY_LAT = 48.048599  # Your latitude
+MY_LONG = 9.329020  # Your longitude
+GMT = 2  # Time difference
